8d89a655ec8d0d2fbb1e6b421e13e34df6bce69a
safnamoleps <safnasiyas99@gmail.com>
2024-02-13 19:29:45 +0530

    backend for doctorapp

---
+++ index.js
@@ -0,0 +1,17 @@
+//import json server
+const jsonServer = require('json-server')
+//creating server app
+const doctorServer = jsonServer.create()
+//setup path for db.json
+const router = jsonServer.router('db.json')
+//return a middleware
+const middleware = jsonServer.defaults()
+//setup port
+const port = 3001
+//use server
+doctorServer.use(middleware)
+doctorServer.use(router)
+//to run port
+doctorServer.listen(port,()=>{
+    console.log('Doctor server listening on port ',port)
+})
